69604024da8ca991337644d1b0c8dd6342e74def
kurniawan <kurni.aawann19@gmail.com>
2024-09-11 23:23:19 +0700

    add packgae joi and validation

---
+++ src/validator/notes/index.js
@@ -0,0 +1,12 @@
+const { NotePayloadSchema } = require("./scheme");
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new Error(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
